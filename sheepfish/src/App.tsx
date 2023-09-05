438579b75aed7f4188219b66d37d3ea0064593f0
Stas Ivko <soltivka@gmail.com>
2023-09-05 21:53:12 +0300

    initial project structure
    Created initial project structure, set up layout, established basic routes, and implemented page components for the project. This commit outlines the project's framework after integrating all required libraries

--- sheepfish/src/App.tsx
+++ sheepfish/src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./layout/Layout/Layout";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
---
+++ sheepfish/src/pages/Products/Products.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Products() {
+  return (
+    <div>
+        products list
+    </div>
+  );
+}
+
+export default Products;
