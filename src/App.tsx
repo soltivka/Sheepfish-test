1350b8b0307035b7500dbbd520b2857db39b0d04
Stas Ivko <soltivka@gmail.com>
2023-09-10 17:40:21 -0400

    setup default index route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import "./App.css";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 import Layout from "./layout/Layout/Layout";
 import Products from "./pages/Products/Products";
-import NotFound from "./pages/NotFound/NotFound";
 import CreateProduct from "./pages/CreateProduct/CreateProduct";
 import Routing from "./routing";
 
@@ -13,7 +12,7 @@ function App() {
         <Routes>
           <Route path={Routing.Products} element={<Products />} />
           <Route path={Routing.CreateProduct} element={<CreateProduct />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Navigate to={Routing.Products} replace />} />
         </Routes>
       </Layout>
     </Router>
--- src/routing.ts
+++ src/routing.ts
@@ -1,5 +1,5 @@
 enum Routing {
-  Products = '/',
+  Products = '/products',
   CreateProduct = '/new'
 }
 
